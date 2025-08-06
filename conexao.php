01d26836ece2cbd18be183e7a8add98e8279ca0e
ThyAllves <thiagocsalves03@gmail.com>
2025-08-06 10:25:12 -0300

    Atualização para banco remoto

--- conexao.php
+++ conexao.php
@@ -1,14 +1,16 @@
 <?php 
 
 // conexao.php
-$host = 'localhost';
-$usuario = 'root';
-$senha = '';
-$database = 'consultas';
+$servidor = 'sql10.freesqldatabase.com';
+$usuario = 'sql10793848';
+$senha = 'qLa5JIqdXG';
+$database = 'sql10793848';
+$porta = 3306;
 
-$conexao = mysqli_connect($host, $usuario, $senha, $database);
-if (!$conexao) {
-    echo "<script>alert('Erro ao conectar ao banco de dados: " . mysqli_connect_error() . "');</script>";
+$conexao = new mysqli($servidor, $usuario, $senha, $database, $porta);
+
+if ($conexao) {
+    echo "<script>alert('Conectado com sucesso!');</script>";
     exit();
 }
 ?>
