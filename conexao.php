e21dcbfbb2d959651a36a569a88b87fd42cf51c3
ThyAllves <thiagocsalves03@gmail.com>
2025-08-06 11:49:45 -0300

    atualização do DB remoto 2.1

--- conexao.php
+++ conexao.php
@@ -7,7 +7,7 @@ $senha = 'qLa5JIqdXG';
 $database = 'sql10793848';
 $porta = 3306;
 
-$conexao = new mysqli($servidor, $usuario, $senha, $database, $porta);
+$conexao = mysqli_init($servidor, $usuario, $senha, $database, $porta);
 
 if ($conexao) {
     echo "<script>alert('Conectado com sucesso!');</script>";
