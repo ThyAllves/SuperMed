cf3e88bf0cb675d3c7bc976bd311f5fa2d5c7e56
ThyAllves <thiagocsalves03@gmail.com>
2025-08-06 11:38:06 -0300

    Atualização do DB remoto 2.0

--- conexao.php
+++ conexao.php
@@ -1,20 +1,16 @@
-<?php
+<?php 
+
+// conexao.php
 $servidor = 'sql10.freesqldatabase.com';
 $usuario = 'sql10793848';
 $senha = 'qLa5JIqdXG';
 $database = 'sql10793848';
 $porta = 3306;
 
-// Criar conexão
-$conexao = mysqli_init();
+$conexao = new mysqli($servidor, $usuario, $senha, $database, $porta);
 
-// Adiciona SSL vazio (resolve problema com alguns bancos gratuitos)
-mysqli_ssl_set($conexao, NULL, NULL, NULL, NULL, NULL);
-
-// Tenta conectar
-if (mysqli_real_connect($conexao, $servidor, $usuario, $senha, $database, $porta, NULL, MYSQLI_CLIENT_SSL)) {
-    echo "✅ Conectado com sucesso!";
-} else {
-    echo "❌ Erro: " . mysqli_connect_error();
+if ($conexao) {
+    echo "<script>alert('Conectado com sucesso!');</script>";
+    exit();
 }
 ?>
