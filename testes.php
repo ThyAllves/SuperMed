eab9ebac4629ef9e352aac33ee21b5537407883a
ThyAllves <thiagocsalves03@gmail.com>
2025-08-07 11:35:13 -0300

    phpinfo()

--- testes.php
+++ testes.php
@@ -1,12 +1,4 @@
 <?php 
-
-if(function_exists('mysqli_connect')) {
-    include 'conexao.php';
-} else {
-    echo "<script>alert('A extensão MySQLi não está habilitada.');</script>";
-    
-}
- 
 phpinfo();
 
 ?>
